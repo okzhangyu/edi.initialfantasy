b6e42e86729c3f26cce0932b54236c7bfef9c2da
fancys <fancy553973204@gmail.com>
2018-05-31 17:51:53 +0800

    删除用户服务无效api

--- src/main/java/org/edi/initialfantasy/service/UserService.java
+++ src/main/java/org/edi/initialfantasy/service/UserService.java
@@ -84,10 +84,4 @@ public class UserService implements IUserService{
     }
 
 
-    @GET
-    @Path("/getname")
-    @Produces("text/plain")
-    public String UserLogin(){
-        return "hello";
-    }
 }
