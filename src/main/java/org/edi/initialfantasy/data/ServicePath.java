0f09932cba7bcd11e735866fb2fbf77c347dcc89
fancys <fancy553973204@gmail.com>
2018-07-26 14:34:32 +0800

    添加条码解析服务

--- src/main/java/org/edi/initialfantasy/data/ServicePath.java
+++ src/main/java/org/edi/initialfantasy/data/ServicePath.java
@@ -7,7 +7,9 @@ package org.edi.initialfantasy.data;
  */
 
 public class ServicePath {
+
     public static final String LOGIN_SERVICE_ROUTE = "v1/userauthrization";
 
     public static final String TOKEN_NAMER = "token";
+
 }
