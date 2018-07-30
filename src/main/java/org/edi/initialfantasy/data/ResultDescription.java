5fea08602fb57e5e42413429b9a3a0aff5278073
fancys <fancy553973204@gmail.com>
2018-07-30 13:14:11 +0800

    添加单元测试

--- src/main/java/org/edi/initialfantasy/data/ResultDescription.java
+++ src/main/java/org/edi/initialfantasy/data/ResultDescription.java
@@ -14,25 +14,23 @@ public class ResultDescription extends OpResultDescription {
 
     public static final String DOCENTRY_IS_NULL ="the value of docentry is empty";
 
-    public static String TOKEN_IS_EMPTY = "the value of token is empty";
+    public static final String TOKEN_IS_EMPTY = "the value of token is empty";
 
-    public static String TOKEN_IS_EXPIRED = "the token has expired";
+    public static final String TOKEN_IS_EXPIRED = "the token has expired";
 
-    public static String TOKEN_IS_ERROR = "the value of token is error";
+    public static final String TOKEN_IS_ERROR = "the value of token is error";
 
-    public static  String COMPANY_IS_NONEXISTENT = "the company is not existent";
+    public static final String COMPANY_IS_NONEXISTENT = "the company is not existent";
 
-    public static  String USER_IS_NONEXISTENT = "the user is not existent";
+    public static final String USER_IS_NONEXISTENT = "the user is not existent";
 
-    public static  String USERPASSWORD_IS_ERROR = "the password of user is error";
+    public static final String USERPASSWORD_IS_ERROR = "the password of user is error";
 
-    public static  String B1DOCENTRY_IS_EXISTENT = "the bill have been generated";
+    public static final String B1DOCENTRY_IS_EXISTENT = "the bill have been generated";
 
-    public static  String PARAMETER_IS_NULL= "the parameter info is null";
+    public static final String PARAMETER_IS_NULL= "the parameter info is empty";
 
-    public static  String CODEBAR_IS_NULL= "the codebar info is null";
+    public static final String CODEBAR_IS_NULL= "the codebar info is empty";
 
-    public static  String DOCENTRY_IS_NULL= "the docEntry info is null";
-
-    public static  String DOCTYPE_IS_NULL= "the docType info is null";
+    public static final String DOCTYPE_IS_NULL= "the docType info is empty";
 }
