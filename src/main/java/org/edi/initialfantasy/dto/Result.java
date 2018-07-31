d52efbe67b7607f6c3718192a356094d8f2efc39
fancys <fancy553973204@gmail.com>
2018-07-31 19:48:13 +0800

    调整服务

--- src/main/java/org/edi/initialfantasy/dto/Result.java
+++ src/main/java/org/edi/initialfantasy/dto/Result.java
@@ -1,5 +1,7 @@
 package org.edi.initialfantasy.dto;
 
+import org.edi.freamwork.bo.BusinessObjectException;
+
 import java.util.List;
 
 public class Result<T> implements IResult<T>{
@@ -50,6 +52,10 @@ public class Result<T> implements IResult<T>{
     public Result(String code,Exception e){
         this.code = code;
         this.message = "failed:"+(e.getCause()==null?e.getMessage():e.getCause().toString());
-        this.data = null;
+    }
+
+    public Result(BusinessObjectException e){
+        this.code = e.getCode();
+        this.message =e.getMessage();
     }
 }
