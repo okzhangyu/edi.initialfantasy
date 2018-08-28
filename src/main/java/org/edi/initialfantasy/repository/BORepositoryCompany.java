187cc11afc0d8fe0ebfe5e8b6b6f84ccece7bb47
comeyoung <18311448784@163.com>
2018-08-28 09:33:10 +0800

    添加日志

--- src/main/java/org/edi/initialfantasy/repository/BORepositoryCompany.java
+++ src/main/java/org/edi/initialfantasy/repository/BORepositoryCompany.java
@@ -5,7 +5,10 @@ import org.edi.freamwork.data.operation.OpResultDescription;
 import org.edi.freamwork.exception.BusinessException;
 import org.edi.freamwork.exception.DBException;
 import org.edi.initialfantasy.bo.company.Company;
+import org.edi.initialfantasy.data.ResultDescription;
 import org.edi.initialfantasy.mapper.CompanyMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -15,6 +18,8 @@ import org.springframework.stereotype.Component;
 
 @Component(value="boRepositoryCompany")
 public class BORepositoryCompany implements IBORepositoryCompany {
+    Logger logger = LoggerFactory.getLogger(BORepositoryCompany.class);
+
     @Autowired
     private CompanyMapper companyMapper;
 
@@ -30,8 +35,10 @@ public class BORepositoryCompany implements IBORepositoryCompany {
             Company company =companyMapper.serchCompanyId(companyName);
             return company;
         } catch (BusinessException e){
+            logger.info(ResultDescription.LOGIN_EXCEPTION,e);
             throw e;
         }catch (Exception e){
+            logger.info(ResultDescription.LOGIN_EXCEPTION,e);
             throw new DBException(OpResultCode.DATABASE_OPERATE_ERROR,OpResultDescription.DATABASE_OPERATE_ERROR);
         }
 
